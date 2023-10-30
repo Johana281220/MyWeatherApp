69a5034b89c07e1ba5b72738da4e8f66b34dcc7f
Sonila Mihali <138469013+sonilamihali@users.noreply.github.com>
2023-10-30 09:10:37 +0100

    Update LocationService.java

--- src/main/java/org/sda/service/LocationService.java
+++ src/main/java/org/sda/service/LocationService.java
@@ -4,14 +4,7 @@ import org.hibernate.id.UUIDGenerationStrategy;
 import org.sda.model.Location;
 
 import java.util.List;
-import java.util.UUID;
-
-//Service interface si LocationService përfaqëson business logic të nivelit të lartë ose operacionet
-//specifike të aplikacionit që lidhen me një domain të caktuar, siç është vendndodhja në kete rast
-//Përmbledh logjikën e biznesit, vërtetimin dhe koordinimin midis operacioneve të shumëfishta të
-//aksesit të të dhënave.
-//Mund të përdorë një ose më shumë objekte të aksesit të të dhënave (DAO) për të bashkëvepruar me
-//burimin themelor të të dhënave (p.sh., një bazë të dhënash).
+import java.util.UUID
 public interface LocationService {
     public Location getLocationById(UUID id);
     public List<Location> getAllLocation(Location location);
